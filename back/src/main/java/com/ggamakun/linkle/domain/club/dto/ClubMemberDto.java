160067ab597782b11b29a7dccd9a887290b91554
nawhgnues <nawhgnues1313@gmail.com>
2025-10-22 11:17:18 +0900

    54th commit

---
+++ back/src/main/java/com/ggamakun/linkle/domain/club/dto/ClubMemberDto.java
@@ -0,0 +1,22 @@
+package com.ggamakun.linkle.domain.club.dto;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClubMemberDto {
+    private Integer clubId;
+    private Integer memberId;
+    private String nickname;
+    private String fileLink;
+    private String role;
+    private String status;
+    private Timestamp joinedAt;
+    private String rejectionReason;
+}
