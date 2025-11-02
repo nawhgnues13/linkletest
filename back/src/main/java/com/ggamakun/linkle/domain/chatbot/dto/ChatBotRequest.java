615119ef0fd5e5af1fa9379c3a8be60f71e4e3aa
nawhgnues <nawhgnues1313@gmail.com>
2025-11-03 08:57:06 +0900

    84th commit

--- back/src/main/java/com/ggamakun/linkle/domain/chatbot/dto/ChatBotRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ggamakun.linkle.domain.chatbot.dto;
-
-import lombok.Data;
-
-@Data
-public class ChatBotRequest {
-    private String message;
-    private Integer memberId; // 사용자 맥락 조회용
-}
